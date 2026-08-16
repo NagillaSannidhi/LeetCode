cc35c7db9c774e99c1b2d0f1d779403ebca7ecd0
Sannidhi Nagilla <sannidhinagilla@gmail.com>
2026-08-16 11:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (15.87%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -14,9 +14,9 @@ class Solution {
         
         while (current != null && current.next != null) {
             if (current.val == current.next.val) {
-                current.next = current.next.next; // skip the duplicate
+                current.next = current.next.next;
             } else {
-                current = current.next; // move forward
+                current = current.next; // move
             }
         }
         
