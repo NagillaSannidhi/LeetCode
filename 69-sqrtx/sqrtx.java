693f5fe843aaebb1acc37853cb5baa7584a414ba
Sannidhi Nagilla <sannidhinagilla@gmail.com>
2026-08-16 12:04:43 +0530

    Time: 1 ms (98.13%) | Memory: 42.7 MB (23.83%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        
+        long left = 1, right = x / 2;
+        long ans = 1;
+        
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long sq = mid * mid;
+            
+            if (sq == x) {
+                return (int) mid;
+            } else if (sq < x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return (int) ans;
+        
+    }
+}
