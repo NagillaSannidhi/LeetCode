bbd7e262abcc63343d2167054d3f7126716f71c4
Sannidhi Nagilla <sannidhinagilla@gmail.com>
2026-08-28 21:34:46 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (83.99%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> result = new ArrayList<>();
+        backtrack(s.toCharArray(), 0, result);
+        return result;
+    }
+
+    private void backtrack(char[] chars, int index, List<String> result) {
+        if (index == chars.length) {
+            result.add(new String(chars));
+            return;
+        }
+        backtrack(chars, index + 1, result);
+        if (Character.isLetter(chars[index])) {
+            chars[index] ^= 1 << 5;
+            backtrack(chars, index + 1, result);
+            chars[index] ^= 1 << 5; 
+        }
+    }
+}
