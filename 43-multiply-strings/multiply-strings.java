c064c2150f32b84a31d47c480756209dea81aa7f
Sannidhi Nagilla <sannidhinagilla@gmail.com>
2026-09-04 18:09:57 +0530

    Time: 3 ms (78.36%) | Memory: 43.4 MB (80.29%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        int m = num1.length(), n = num2.length();
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            int digit1 = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int digit2 = num2.charAt(j) - '0';
+                int product = digit1 * digit2;
+
+                int lowPos = i + j + 1;
+                int highPos = i + j;
+
+                int sum = product + result[lowPos];
+                result[lowPos] = sum % 10;
+                result[highPos] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int digit : result) {
+            if (!(sb.length() == 0 && digit == 0)) { // skip leading zeros
+                sb.append(digit);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+        
+    }
+}
